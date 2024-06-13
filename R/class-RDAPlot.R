d41c3068433359690cc2f40011890dc4db9e72f0
seraidarian <ely.seraidarian@ecole.ensicaen.fr>
2024-06-13 14:15:36 +0300

    Added RDAPlot visual parameters with group checkbox

--- R/class-RDAPlot.R
+++ R/class-RDAPlot.R
@@ -16,6 +16,14 @@
 #' \item \code{vec.text}, a logical indicating if text should be encased in a box.
 #' \item \code{confidence.level}, a numeric between 0 and 1 to adjust confidence level.
 #' \item \code{ellipse.alpha}, a numeric between 0 and 1 t o adjust ellipse opacity.
+#' \item \code{ellipse.linewidth}, a numeric specifying the size of ellipses.
+#' \item \code{ellipse.linetype}, a numeric specifying the style of ellipses.
+#' \item \code{vec.size}, a numeric specifying the size of vectors.
+#' \item \code{vec.colour}, a string specifying the colour of vectors.
+#' \item \code{vec.linetype}, a numeric specifying the style of vector lines.
+#' \item \code{arrow.size}, a numeric specifying the size of arrows.
+#' \item \code{label.colour}, a string specifying the colour of text and labels.
+#' \item \code{label.size}, a numeric specifying the size of text and labels. 
 #' \item \code{add.significance}, a logical indicating if variance and p-value
 #'  should appear in the labels.
 #' \item \code{add.expl.var}, a logical indicating if variance shoud appear 
@@ -60,18 +68,31 @@ setClassUnion("charlog", c("character", "logical"))
 setClass("RDAPlot", contains="Panel", slots=c(dimred="character",
     add.ellipse="charlog", colour_by="character", vec.text="logical",
     add.vectors="logical", ellipse.alpha="numeric", confidence.level="numeric",
-    add.significance="logical", add.expl.var="logical"))
+    add.significance="logical", add.expl.var="logical", ellipse.linewidth="numeric",
+    ellipse.linetype="numeric", vec.size="numeric", vec.colour="character",
+    vec.linetype="numeric", arrow.size="numeric", label.colour="character",
+    label.size="numeric", vector_parameters="logical", visual_parameters="character",
+    arrow_parameters="logical", ellipse_parameters="logical", label_parameters="logical"))
 
 #' @importFrom iSEE .singleStringError .validLogicalError
 #' @importFrom S4Vectors setValidity2
 setValidity2("RDAPlot", function(x) {
     msg <- character(0)
     
-    msg <- .singleStringError(msg, x, fields=c("dimred", "colour_by"))
+    msg <- .singleStringError(msg, x, fields=c("dimred", "colour_by", "vec.colour",
+                                    "label.colour", "visual_parameters"))
     msg <- .validLogicalError(msg, x, fields=c("vec.text", "add.vectors",
-                                    "add.significance", "add.expl.var"))
+                                    "add.significance", "add.expl.var",
+                                    "vector_parameters", "ellipse_parameters",
+                                    "arrow_parameters", "label_parameters"))
     msg <- .validNumberError(msg, x, "ellipse.alpha", lower=0, upper=1)
     msg <- .validNumberError(msg, x, "confidence.level", lower=0, upper=1)
+    msg <- .validNumberError(msg, x, "ellipse.linewidth", lower=0, upper=1)
+    msg <- .validNumberError(msg, x, "ellipse.linetype", lower=1, upper=6)
+    msg <- .validNumberError(msg, x, "vec.size", lower=0, upper=1)
+    msg <- .validNumberError(msg, x, "vec.linetype", lower=1, upper=6)
+    msg <- .validNumberError(msg, x, "arrow.size", lower=0, upper=1)
+    msg <- .validNumberError(msg, x, "label.size", lower=0, upper=10)
     
     
     if( length(msg) ){
@@ -86,14 +107,27 @@ setValidity2("RDAPlot", function(x) {
 setMethod("initialize", "RDAPlot", function(.Object, ...) {
     args <- list(...)
     args <- .emptyDefault(args, "dimred", "RDA")
+    args <- .emptyDefault(args, "vec.colour", "black")
+    args <- .emptyDefault(args, "label.colour", "black")
     args <- .emptyDefault(args, "add.ellipse", "fill")
     args <- .emptyDefault(args, "colour_by", NA_character_)
     args <- .emptyDefault(args, "add.vectors", TRUE)
     args <- .emptyDefault(args, "vec.text", TRUE)
+    args <- .emptyDefault(args, "vector_parameters", FALSE)
+    args <- .emptyDefault(args, "visual_parameters", NA_character_)
+    args <- .emptyDefault(args, "arrow_parameters", FALSE)
+    args <- .emptyDefault(args, "ellipse_parameters", FALSE)
+    args <- .emptyDefault(args, "label_parameters", FALSE)
     args <- .emptyDefault(args, "add.expl.var", TRUE)
     args <- .emptyDefault(args, "add.significance", TRUE)
     args <- .emptyDefault(args, "ellipse.alpha", 0.2)
     args <- .emptyDefault(args, "confidence.level", 0.95)
+    args <- .emptyDefault(args, "ellipse.linewidth", 0.1)
+    args <- .emptyDefault(args, "ellipse.linetype", 1)
+    args <- .emptyDefault(args, "vec.size", 0.5)
+    args <- .emptyDefault(args, "vec.linetype", 1)
+    args <- .emptyDefault(args, "arrow.size", 0.25)
+    args <- .emptyDefault(args, "label.size", 4)
     
     do.call(callNextMethod, c(list(.Object), args))
 })
@@ -136,7 +170,10 @@ setMethod(".createObservers", "RDAPlot",
     
     .createProtectedParameterObservers(panel_name, c("dimred", "add.ellipse",
         "colour_by", "vec.text", "add.vectors", "add.expl.var", "add.significance",
-        "confidence.level", "ellipse.alpha"),
+        "confidence.level", "ellipse.alpha", "ellipse.linewidth", "ellipse.linetype",
+        "vec.size", "vec.colour", "vec.linetype", "arrow.size", "label.colour",
+        "label.size", "vector_parameters", "arrow_parameters", "visual_parameters",
+        "ellipse_parameters", "label_parameters"),
         input=input, pObjects=pObjects, rObjects=rObjects)
     
     invisible(NULL)
@@ -176,14 +213,34 @@ setMethod(".generateOutput", "RDAPlot",
     }
 
     args[["dimred"]] <- deparse(slot(x, "dimred"))
-    args[["add.ellipse"]] <- deparse(slot(x, "add.ellipse"))
     args[["colour_by"]] <- deparse(slot(x, "colour_by"))
-    args[["vec.text"]] <- deparse(slot(x, "vec.text"))
-    args[["add.vectors"]] <- deparse(slot(x, "add.vectors"))
-    args[["ellipse.alpha"]] <- deparse(slot(x, "ellipse.alpha"))
     args[["confidence.level"]] <- deparse(slot(x, "confidence.level"))
     args[["add.expl.var"]] <- deparse(slot(x, "add.expl.var"))
     args[["add.significance"]] <- deparse(slot(x, "add.significance"))
+    
+    if( slot(x, "vector_parameters") == TRUE ){
+        args[["vec.size"]] <- deparse(slot(x, "vec.size"))
+        args[["vec.colour"]] <- deparse(slot(x, "vec.colour"))
+        args[["vec.linetype"]] <- deparse(slot(x, "vec.linetype"))
+        args[["vec.text"]] <- deparse(slot(x, "vec.text"))
+        args[["add.vectors"]] <- deparse(slot(x, "add.vectors"))
+    }
+    
+    if( slot(x, "arrow_parameters") == TRUE ){
+        args[["arrow.size"]] <- deparse(slot(x, "arrow.size"))
+    }
+    
+    if( slot(x, "ellipse_parameters") == TRUE ){
+        args[["ellipse.linewidth"]] <- deparse(slot(x, "ellipse.linewidth"))
+        args[["ellipse.linetype"]] <- deparse(slot(x, "ellipse.linetype"))
+        args[["ellipse.alpha"]] <- deparse(slot(x, "ellipse.alpha"))
+        args[["add.ellipse"]] <- deparse(slot(x, "add.ellipse"))
+    }
+    
+    if( slot(x, "label_parameters") == TRUE ){
+        args[["label.colour"]] <- deparse(slot(x, "label.colour"))
+        args[["label.size"]] <- deparse(slot(x, "label.size"))
+    }
     
     args <- sprintf("%s=%s", names(args), args)
     args <- paste(args, collapse=", ")
@@ -293,19 +350,89 @@ setMethod(".definePanelTour", "RDAPlot", function(x) {
         data.frame(rbind(c(element = paste0("#", panel_name,
             "_add\\.significance"), intro = "Here, we can choose
             whether or not to add variance in the axes.")))})
+    .addSpecificTour(class(x)[1], "ellipse.linewidth", function(panel_name) {
+        data.frame(rbind(c(element = paste0("#", panel_name,
+            "_ellipse\\.linewidth"), intro = "Here, we can adjust
+            the size of outline ellipses.")))})
+    .addSpecificTour(class(x)[1], "ellipse.linetype", function(panel_name) {
+        data.frame(rbind(c(element = paste0("#", panel_name,
+            "_ellipse\\.linetype"), intro = "Here, we can adjust
+            the style of outline ellipses.")))})
+    .addSpecificTour(class(x)[1], "vec.size", function(panel_name) {
+        data.frame(rbind(c(element = paste0("#", panel_name,
+            "_vec\\.size"), intro = "Here, we can adjust
+            the vectors thickness.")))})
+    .addSpecificTour(class(x)[1], "vec.colour", function(panel_name) {
+        data.frame(rbind(c(element = paste0("#", panel_name,
+            "_vec\\.colour + .selectize-control"), intro = "Here, we can adjust
+            the vectors colour.")))})
+    .addSpecificTour(class(x)[1], "vec.linetype", function(panel_name) {
+        data.frame(rbind(c(element = paste0("#", panel_name,
+            "_vec\\.linetype"), intro = "Here, we can adjust
+            the vectors style")))})
+    .addSpecificTour(class(x)[1], "arrow.size", function(panel_name) {
+        data.frame(rbind(c(element = paste0("#", panel_name,
+            "_arrow\\.size"), intro = "Here, we can adjust
+            the arrows size")))})
+    .addSpecificTour(class(x)[1], "label.colour", function(panel_name) {
+        data.frame(rbind(c(element = paste0("#", panel_name,
+            "_label\\.colour + .selectize-control"), intro = "Here, we can adjust
+            the labels colour")))})
+    .addSpecificTour(class(x)[1], "label.size", function(panel_name) {
+        data.frame(rbind(c(element = paste0("#", panel_name,
+            "_label\\.size"), intro = "Here, we can adjust
+            the labels size")))})
+    .addSpecificTour(class(x)[1], "visual_parameters", function(panel_name) {
+        data.frame(rbind(c(element = paste0("#", panel_name,
+            "_visual_parameters"), intro = "Here, we can choose
+            the visual parameters to adjust")))})
+    
     
     # Define what parameters the user can adjust
     collapseBox(paste0(panel_name, "_Visual"),
         title="Visual parameters", open=FALSE,
+        
+        .checkboxGroupInput.iSEE(x, field="visual_parameters", label="Visual parameters:",
+            inline=TRUE, selected=slot(x, "visual_parameters"),
+            choices=c("Vector", "Arrow", "Ellipse", "Label")),
         .selectInput.iSEE(x, field="colour_by", label="Color by",
             choices=names(colData(se)), selected=slot(x, "colour_by")),
-        .selectInput.iSEE(x, field="add.ellipse", label="Ellipse style",
-            choices = c("fill", "colour", "FALSE"),
-            selected=slot(x, "add.ellipse")),
-        .sliderInput.iSEE(x, field="ellipse.alpha", label="Ellipse opacity",
-            min=0.01, max=0.99, step=0.01, value=slot(x, "ellipse.alpha")),
-        .checkboxInput.iSEE(x, field="add.vectors", label="Add vectors",
-            value=slot(x, "add.vectors")),
+        .conditionalOnCheckGroup(
+            paste0(panel_name, "_visual_parameters"), "Arrow",
+                .sliderInput.iSEE(x, field="arrow.size", label="Arrows size",
+                    min=0.01, max=0.99, step=0.01, value=slot(x, "arrow.size"))),
+        .conditionalOnCheckGroup(
+            paste0(panel_name, "_visual_parameters"), "Label",
+            list(
+                .selectInput.iSEE(x, field="label.colour", label="Labels colour",
+                    choices=c("black","purple","pink","blue","green","red",
+                    "yellow","orange"), selected=slot(x, "label.colour")),
+                .sliderInput.iSEE(x, field="label.size", label="Labels size",
+                    min=2.5, max=5.5, step=0.1, value=slot(x, "label.size")))),
+        .conditionalOnCheckGroup(
+          paste0(panel_name, "_visual_parameters"), "Ellipse",
+          list(
+              .selectInput.iSEE(x, field="add.ellipse", label="Ellipse style",
+                  choices = c("fill", "colour", "FALSE"),
+                  selected=slot(x, "add.ellipse")),
+              .sliderInput.iSEE(x, field="ellipse.alpha", label="Ellipse opacity",
+                  min=0.01, max=0.99, step=0.01, value=slot(x, "ellipse.alpha")),
+              .sliderInput.iSEE(x, field="ellipse.linewidth", label="Ellipse outline size",
+                  min=0.01, max=0.99, step=0.01, value=slot(x, "ellipse.linewidth")),
+              .numericInput.iSEE(x, field="ellipse.linetype", label="Ellipse outline style",
+                  value=slot(x, "ellipse.linetype"), min=1, max=6, step=1))),
+        .conditionalOnCheckGroup(
+            paste0(panel_name, "_visual_parameters"), "Vector",
+            list(
+                .checkboxInput.iSEE(x, field="add.vectors", label="Add vectors",
+                    value=slot(x, "add.vectors")),
+                .sliderInput.iSEE(x, field="vec.size", label="Vectors thickness",
+                    min=0.01, max=0.99, step=0.01, value=slot(x, "vec.size")),
+                .selectInput.iSEE(x, field="vec.colour", label="Vectors colour",
+                    choices=c("black","purple","pink","blue","green","red",
+                    "yellow","orange"), selected=slot(x, "vec.colour")),
+                .numericInput.iSEE(x, field="vec.linetype", label="Vectors style",
+                    value=slot(x, "vec.linetype"), min=1, max=6, step=1))),
         .conditionalOnCheckSolo(paste0(panel_name, "_add.vectors"), TRUE,
             .checkboxInput.iSEE(x, field="vec.text", label="Unboxed labels",
                 value=slot(x, "vec.text"))))
