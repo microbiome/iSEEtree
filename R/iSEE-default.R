855c091113021f9750013f0985a0f72766eb0c59
Giulio Benedetti <giuliobene2000@gmail.com>
2024-07-16 09:31:30 +0300

    Generate panels when elements are available + warnings

--- R/iSEE-default.R
+++ R/iSEE-default.R
@@ -52,6 +52,8 @@ setGeneric("iSEE", iSEE::iSEE)
 #'   ComplexHeatmapPlot RowDataTable ColumnDataTable
 #' @importFrom TreeSummarizedExperiment TreeSummarizedExperiment rowLinks
 #' @importFrom SingleCellExperiment reducedDims
+#' @importFrom SummarizedExperiment rowData colData
+#' @importFrom mia taxonomyRanks
 setMethod("iSEE", "TreeSummarizedExperiment",
     function(se, initial = c(RowDataTable(), ColumnDataTable(), RowTreePlot(),
         AbundancePlot(), AbundanceDensityPlot(), ReducedDimensionPlot(),
@@ -59,10 +61,13 @@ setMethod("iSEE", "TreeSummarizedExperiment",
         landingPage = createLandingPage(), tour = NULL, appTitle = NULL,
         runLocal = TRUE, voice = FALSE, bugs = FALSE, saveState = NULL, ...) {
     
+    initial <- .check_panel(se, initial, "RowDataTable", rowData)
+    initial <- .check_panel(se, initial, "ColumnDataTable", colData)
     initial <- .check_panel(se, initial, "RowTreePlot", rowLinks)
+    initial <- .check_panel(se, initial, "AbundancePlot", taxonomyRanks)
     initial <- .check_panel(se, initial, "ReducedDimensionPlot", reducedDims)
 
-    iSEE::iSEE(se, initial = initial, extra = extra, colormap = colormap,
+    iSEE::iSEE(se, initial = initial, extra = initial, colormap = colormap,
         landingPage = landingPage, tour = tour, appTitle = appTitle,
         runLocal = runLocal, voice = voice, bugs = bugs,
         saveState = saveState, ...)
@@ -70,12 +75,14 @@ setMethod("iSEE", "TreeSummarizedExperiment",
 
 #' @importFrom S4Vectors isEmpty
 #' @importFrom methods is
-.check_panel <- function(se, panel_list, panel_class, panel_fun) {
+.check_panel <- function(se, panel_list, panel_class, panel_fun, wtext) {
     
     no_keep <- unlist(lapply(panel_list, function(x) is(x, panel_class)))
 
     if( any(no_keep) && (is.null(panel_fun(se)) || isEmpty(panel_fun(se))) ){
         panel_list <- panel_list[!no_keep]
+        warning("no valid ", as.character(substitute(panel_fun)),
+            " fields for ", panel_class, call. = FALSE)
     }
     
     return(panel_list)
