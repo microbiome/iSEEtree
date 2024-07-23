9cd27668d141b0d664078c2b3973e80726ea2de8
Giulio Benedetti <giuliobene2000@gmail.com>
2024-07-23 12:17:53 +0300

    Fix docs

--- R/iSEEtree-package.R
+++ R/iSEEtree-package.R
@@ -3,16 +3,17 @@
 #' `iSEEtree` is an extension of \pkg{iSEE} that provides panels for the
 #' \code{\link[TreeSummarizedExperiment:TreeSummarizedExperiment-constructor]{TreeSummarizedExperiment}}
 #' container, enabling the interactive visualisation of typical microbiome data.
-#' The panel layout of iSEEtree is described in \link[iSEE].
-#'
-#' @author Giulio Benedetti \email{giulio.benedetti@@utu.fi}
-#'
-#' @name iSEEtree
-#' @docType package
-#' @keywords internal
+#' The panel layout of iSEEtree is described in \code{\link{iSEE}}.
 #'
 #' @examples
 #' library(iSEEtree)
 #' 
-#' @seealso [\link[iSEE:iSEE]{iSEE} \link[mia:mia]{mia}]
+#' @seealso \link[iSEE:iSEE]{iSEE} \link[mia:mia]{mia}
+#' 
+#' @author Giulio Benedetti \email{giulio.benedetti@@utu.fi}
+#' 
+#' @docType package
+#' @keywords internal
+#' @name iSEEtree
+#' 
 "_PACKAGE"
